575abb900f63e31e7688c06b1a7a015cf93c0ca9
Nithish_naani <nithishnani1609@gmail.com>
2023-06-13 15:51:28 +0000

    done

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+import BlogList from '../BlogList'
+import UserInfo from '../UserInfo'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+export default Home
